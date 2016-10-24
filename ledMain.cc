57de7f27bb057b8e97a0c8b48452ceb9cb23c4e1
Fabio Nunes Garcia <binho9_7@hotmail.com>
2016-10-24 20:29:23 -0200

    Simple menu for RGB leds

--- ledMain.cc
+++ ledMain.cc
@@ -15,11 +15,67 @@ GPIO ledB('C', 0, EPOS::GPIO::OUTPUT);
 OStream cout;
 int main(int argc, char** argv) {
     while(true) {
+    	cout<< "Waiting for input\n";
+    	unsigned long a = USB::get();
+    	/*
+    	//Turn off all LEDs
+    	case 48:
+    	ledR.set(false);
+    	ledG.set(false);
+    	ledB.set(false);
+    	break;
+    	//Turn on all LEDs
+    	case 49:
     	ledR.set(true);
     	ledG.set(true);
     	ledB.set(true);
-    	cout<< "All LEDs on, waiting for input\n";
-    	unsigned long a = USB::get();
+    	break;
+    	//Invert all LEDs values
+    	case 50:
+    	ledR.set(!ledR.get());
+    	ledG.set(!ledG.get());
+    	ledB.set(!ledB.get());
+    	break;
+    	//Try to simulate (software) PWM on all LEDs
+    	case 51:
+    	unsigned long period = 1;
+    	while(period <= 1000) {
+    	ledR.set(!ledR.get());
+    	ledG.set(!ledG.get());
+    	ledB.set(!ledB.get());
+    	sleep(1);
+    	period++;
+    	}
+    	break;
+    	//Turn on R, G, then B for 100ms each
+    	case 52:
+    	ledR.set(false);
+    	ledG.set(false);
+    	ledB.set(false);
+    	ledR.set(true);
+    	sleep(100);
+    	ledR.set(false);
+    	ledG.set(true);
+    	sleep(100);
+    	ledG.set(false);
+    	ledB.set(true);
+    	sleep(100);
+    	ledB.set(false);
+    	//Turn on B, G, then R for 100ms each
+    	case 53:
+        ledR.set(false);
+    	ledG.set(false);
+    	ledB.set(false);
+    	ledB.set(true);
+    	sleep(100);
+    	ledB.set(false);
+    	ledG.set(true);
+    	sleep(100);
+    	ledG.set(false);
+    	ledR.set(true);
+    	sleep(100);
+    	ledR.set(false);
+    	*/
     	switch (a) {
             case 48:
             ledR.set(false);
