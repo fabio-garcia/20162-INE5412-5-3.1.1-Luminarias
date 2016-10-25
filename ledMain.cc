a2190c6bb007bbd9d224f249ca0bb831180f137f
Fabio Nunes Garcia <binho9_7@hotmail.com>
2016-10-25 04:48:14 -0200

    Implement software PWM

--- ledMain.cc
+++ ledMain.cc
@@ -4,99 +4,127 @@
 #include <gpio.h>
 #include <alarm.h>
 #include <usb.h>
+#include <thread.h>
 
 void sleep(unsigned long);
+int changeBrightness(int intensity);
+int changeBrightnessR(int intensity);
+int changeBrightnessG(int intensity);
+int changeBrightnessB(int intensity);
+int inputIntensity(int input);
 
 using namespace EPOS;
 
 GPIO ledR('C', 3, EPOS::GPIO::OUTPUT);
 GPIO ledG('C', 1, EPOS::GPIO::OUTPUT);
 GPIO ledB('C', 0, EPOS::GPIO::OUTPUT);
+Thread * cbR;
+Thread * cbG;
+Thread * cbB;
+Thread * usbIn;
+int current = 200;
+int input;
 OStream cout;
 int main(int argc, char** argv) {
-    while(true) {
-    	cout<< "Waiting for input\n";
-    	unsigned long a = USB::get();
-    	/*
-    	//Turn off all LEDs
-    	case 48:
-    	ledR.set(false);
-    	ledG.set(false);
-    	ledB.set(false);
-    	break;
-    	//Turn on all LEDs
-    	case 49:
-    	ledR.set(true);
-    	ledG.set(true);
-    	ledB.set(true);
-    	break;
-    	//Invert all LEDs values
-    	case 50:
-    	ledR.set(!ledR.get());
-    	ledG.set(!ledG.get());
-    	ledB.set(!ledB.get());
-    	break;
-    	//Try to simulate (software) PWM on all LEDs
-    	case 51:
-    	unsigned long period = 1;
-    	while(period <= 1000) {
-    	ledR.set(!ledR.get());
-    	ledG.set(!ledG.get());
-    	ledB.set(!ledB.get());
-    	sleep(1);
-    	period++;
-    	}
-    	break;
-    	//Turn on R, G, then B for 100ms each
-    	case 52:
-    	ledR.set(false);
-    	ledG.set(false);
-    	ledB.set(false);
-    	ledR.set(true);
-    	sleep(100);
-    	ledR.set(false);
-    	ledG.set(true);
-    	sleep(100);
-    	ledG.set(false);
-    	ledB.set(true);
-    	sleep(100);
-    	ledB.set(false);
-    	//Turn on B, G, then R for 100ms each
-    	case 53:
-        ledR.set(false);
-    	ledG.set(false);
-    	ledB.set(false);
-    	ledB.set(true);
-    	sleep(100);
-    	ledB.set(false);
-    	ledG.set(true);
-    	sleep(100);
-    	ledG.set(false);
-    	ledR.set(true);
-    	sleep(100);
-    	ledR.set(false);
-    	*/
-    	switch (a) {
-            case 48:
-            ledR.set(false);
-            cout<< a;
-            break;
-			case 49:
-			ledG.set(false);
-			cout<< a;
-			break;
-			case 50:
-			ledB.set(false);
-			cout<< a;
-			break;
-			default:
-			cout<< a;
-			break;
-		}
-        sleep(1000);
-    }
+	usbIn = new Thread(inputIntensity, input);
+	cbR = new Thread(changeBrightnessR, 0);
+	cbG = new Thread(changeBrightnessG, 50);
+	cbB = new Thread(changeBrightnessB, 100);
+	while (true) {
+		usbIn->join();
+		cbR->join();
+		cbG->join();
+		cbB->join();
+	}
+	
 }
 
 void sleep(unsigned long ms) {
-    EPOS::Delay(ms * 1000); //Maintains current LEDs state for -+ms+- milliseconds
+    EPOS::Delay(ms *1); //Holds for -+ms+- microseconds
+}
+
+int inputIntensity(int input) {
+	while(true) {
+	sleep(1000);
+	input = USB::get();
+	cout << input;
+	
+	}
+	return input;
+}
+
+int changeBrightnessR(int intensity) {
+	while (true) {
+    	ledR.set(true);
+    	// Loop over duty cycle
+    	for (int x = 0; x < intensity; x++)
+    	{   
+        	// Set LEDs off when they're on
+        	 if (x > ledR.get())
+        		{
+                	ledR.set(false);
+            	}
+        sleep(intensity);
+    	}
+    	Thread::yield();
+    }
+    return 0;
+}
+
+int changeBrightnessG(int intensity) {
+	while (true) {
+    	ledG.set(true);
+    	// Loop over duty cycle
+    	for (int x = 0; x < intensity; x++)
+    	{   
+        	// Set LEDs off when they're on
+        	 if (x > ledG.get())
+        		{
+                	ledG.set(false);
+            	}
+        sleep(intensity);
+    	}
+    	Thread::yield();
+    }
+    return 0;
+}
+
+int changeBrightnessB(int intensity) {
+	while (true) {
+    	ledB.set(true);
+    	// Loop over duty cycle
+    	for (int x = 0; x < intensity; x++)
+    	{   
+        	// Set LEDs off when they're on
+        	 if (x > ledB.get())
+        		{
+                	ledB.set(false);
+            	}
+        sleep(intensity);
+    	}
+    	Thread::yield();
+    }
+    return 0;
+}
+
+int changeBrightnessAll(int intensity) {
+	while (true) {
+	// Set all LEDs on
+    	ledR.set(true);
+    	ledG.set(true);
+    	ledB.set(true);
+    	// Loop over duty cycle
+    	for (int x = 0; x < intensity; x++)
+    	{   
+        	// Set LEDs off when they're on
+        	 if (x > ledR.get())
+        		{
+                	ledR.set(false);
+    				ledG.set(false);
+    				ledB.set(false);
+            	}
+        sleep(intensity);
+    	}
+    }
+    return 0;
 }
